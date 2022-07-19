ad877cab1aedafd383c6b63971587f6a05d73eee
derekhongg <hongderek@gmail.com>
2022-07-19 15:33:48 -0700

    new frontend updates

--- components/sections/menuItems.js
+++ components/sections/menuItems.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { getStrapiMedia } from "../../utils/media";
+import styles from '../../styles/Navbar.module.css'
+
 function menuItems({data}) {
   return (
     <section class="text-gray-600 body-font">
       <h1 class="text-7xl font-medium title-font text-gray-900 mb-5 text-center py-5">Menu</h1>
+      <div class=" mx-auto text-gray-900 text-center" href="">
+        <a href='./assets/menu.pdf' class={`mr-5 ${styles.linkunderline}`}>View a PDF Menu</a>
+      </div>
   <div class="container px-5 py-20 mx-auto">
     <div class="flex flex-wrap -m-4">
       {data.menuItem.map((item) => {
@@ -13,9 +18,8 @@ function menuItems({data}) {
           <img alt="ecommerce" class="object-cover object-center w-full h-full block" src={`${getStrapiMedia(item.image.data.attributes.url)}`} />
         </a>
         <div class="mt-4">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
           <h2 class="text-gray-900 title-font text-lg font-medium">{item.title}</h2>
-          <p class="mt-1">{item.price}</p>
+          <p class="mt-1">${item.price}</p>
         </div>
       </div>
       )})}
